3f6a90fa821be28659713927c87083ec47c47a20
lsr00ter <codejarry@gmail.com>
2024-08-27 16:13:19 +0800

    udev persistence

---
+++ newjekyllpost.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+filename=$(date +%Y-%m-%d-new-post.md)
+if [ $# -gt 0 ]; then
+  filename=$(date +%Y-%m-%d-)
+  title=$(echo "$@")
+  filename+=$(echo "$@" | sed -e 's/ /-/g').md
+else
+  filename=$(date +%Y-%m-%d-new-post.md)
+fi
+cat >$filename <<EOF
+---
+layout: post
+title: $title
+description: A description
+date: '$(date '+%Y-%m-%d %H:%M:%S')'
+tags: ["tag"]
+---
+EOF
